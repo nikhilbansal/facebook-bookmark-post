064ce25dcffcc7ffaaaf8ff9b01651d2e12bc91d
Deepak Modak <deepak@ozonemedia.com>
2013-12-22 19:48:16 +0530

    added all post menu

--- popup.js
+++ popup.js
@@ -3,20 +3,34 @@ function onchangeFolderLocation(id)
 {
 	chrome.storage.sync.get("bookmark_location", function (obj) {
     		defaultLocation=obj.bookmark_location;
-		console.debug("before setting def loc : "+ defaultLocation);			
-		
+		//console.debug("before setting def loc : "+ defaultLocation);			
 		chrome.storage.sync.set({"bookmark_location":id}, function () {});
 		chrome.storage.sync.get("bookmark_location", function (obj) {
 	    		defaultLocation=obj.bookmark_location;
-			console.debug("after setting def loc : "+ defaultLocation);	
+			//console.debug("after setting def loc : "+ defaultLocation);	
 		});	
 	});
 }
 var folderlist="<select id='folderlocation' class='ask' >";
 var foldername ="";
-function printBookmarks(id) {
+function printAllBookmarks() {
+	var id="1";
+	var listDiv="<label class='listAll' >";
 	chrome.bookmarks.getChildren(id, function(children) {
-		console.debug(children);
+		//console.debug(children);
+	 	for (i = 0; i < children.length; i++) {
+			if(children[i].url){
+				listDiv += '<p >'+children[i].title+'</p>';
+			}
+  		}
+		listDiv+=listDiv+'</label>';
+		document.getElementById("listAllBookmarks").innerHTML=listDiv;
+ 	});
+}
+function loadBookmarkLocations(id) {
+
+	chrome.bookmarks.getChildren(id, function(children) {
+		//console.debug(children);
 	 	for (i = 0; i < children.length; i++) {
 			if(!children[i].url){
 				if(children[i].id == defaultLocation)  //watever nikil set variable
@@ -39,72 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		if(!defaultLocation){
 			chrome.storage.sync.set({"bookmark_location":"1"}, function () {});
 		}
-		printBookmarks('1'); 
+		loadBookmarkLocations('1'); 
+		printAllBookmarks(); 
 	});
-	
 });
-
-
-
-
-/*chrome.bookmarks.getTree(function(bookmarks) {
-  printBookmarks(bookmarks);
-});
-
-function printBookmarks(bookmarks) {
-  bookmarks.forEach(function(bookmark) {
-    console.debug(bookmark.id + ' - ' + bookmark.title + ' - ' + bookmark.url);
-    if (bookmark.children)
-      printBookmark(bookmark.children);
-  });
-}
-*/
-
-/*
-function dumpBookmarks(query) {
-  var bookmarkTreeNodes = chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-    });
-}
-function dumpTreeNodes(bookmarkNodes, query) {
-  var list = $('<ul>');
-  var i;
-  for (i = 0; i < bookmarkNodes.length; i++) {
-    list.append(dumpNode(bookmarkNodes[i], query));
-    alert("namr "+bookmarkNodes[i].title)
-  }
-
-  return list;
-}
-function dumpNode(bookmarkNode, query) {
-  if (bookmarkNode.title) {
-    if (query && bookmarkNode.children) {
-      if (String(bookmarkNode.title).indexOf(query) == -1) {
-        return $('<span></span>');
-      }
-    }
-	
-		var anchor = $('<a>');
-		anchor.attr('href', bookmarkNode.url);
-		anchor.text(bookmarkNode.title);
-		anchor.click(function() {
-		chrome.tabs.create({url: bookmarkNode.url});
-		});
-
-		var span = $('<span>');
-		span.hover(function() {}, function() {}).append(anchor); 
-  
-  }
-  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
-  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-    li.append(dumpTreeNodes(bookmarkNode.children, query));
-  }
-  return li;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  dumpBookmarks();
-});
-
- //document.getElementById("bookmarks").innerHTML="<h1>helllo</h1>";*/
